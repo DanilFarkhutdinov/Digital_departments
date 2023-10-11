4a52e244448363b5860d6edc9425fd2aa22486f7
DanilFarkhutdinov <wf2018danil@mail.ru>
2023-10-12 01:03:48 +0300

    третий коммит

--- files.py
+++ files.py
@@ -1,3 +1,7 @@
+import os
+from os import path
+from pathlib import Path
+
 file = open("main.py", "r") #rb бинарный
 print(file.read())
 file.close()
@@ -7,7 +11,29 @@ file.close()
 #         print(line.strip())
 #     file.close()
 
-with open ("file.txt", "a+") as file:
-    file.write("123")
+cur_path = os.getcwd()
+files_dir = Path(cur_path) / "files"
+print(files_dir)
+try:
+    files_dir.mkdir()
+except:
+    pass
+
+file_path = files_dir / 'file.txt'
+
+with file_path.open("a+") as file:
+    #file.write("123")
     file.seek(0)
     print(file.read())
+
+print(os.path.exists("file.txt"))
+
+print(Path("file.txt").exists())
+
+print(path.basename(file_path))
+print(path.dirname(file_path))
+print(path.splitext("file.txt"))
+
+print(path.abspath(path.join("..", "testdir")))
+
+print(os.environ.get("TEST", "value"))
